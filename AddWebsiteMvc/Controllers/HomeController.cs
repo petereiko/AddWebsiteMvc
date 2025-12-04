e7cf70e6e44e85915da7f97c965deca855a0600e
Hoyte Edorh <edorhsunday@gmail.com>
2025-12-04 17:19:41 +0100

    updated with partners

--- AddWebsiteMvc/Controllers/HomeController.cs
+++ AddWebsiteMvc/Controllers/HomeController.cs
@@ -18,6 +18,8 @@ namespace AddWebsiteMvc.Controllers
             return View();
         }
 
+       
+
         public IActionResult Privacy()
         {
             return View();
---
+++ AddWebsiteMvc/Controllers/ResourceController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddWebsiteMvc.Controllers
+{
+    public class ResourceController : Controller
+    {
+        public IActionResult FaQ()
+        {
+            return View();
+        }
+
+        public IActionResult Careers()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs() 
+        {
+            return View();
+        }
+    }
+}
