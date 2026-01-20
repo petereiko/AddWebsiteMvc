4459649393e96eb15ff1a2690714a45ff50bea8b
peikore <peterayebhere@gmail.com>
2026-01-20 14:48:09 +0100

    ok

---
+++ AddWebsiteMvc/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AddWebsiteMvc.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Promotions()
+        {
+            return View();
+        }
+    }
+}
